26a81d5d7a65237341ba60e0b3dd0b3a166da10e
Javier Ortega Zepeda <j.ortega11@ufromail.cl>
2024-04-25 20:18:13 -0400

    generar json de issues

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 import { Octokit } from "octokit";
 import dotenv from "dotenv";
+import * as fs from 'fs';
+import path from 'path';
+//import hola from ""
 
 dotenv.config();
-
+let issuesData = [];
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
 
 const obtenerIssues = async () => {
     let issues = [];
+    let mensaje;
     for (let index = 0; index < 36; index++) {
-        issues.push(await octokit.request("GET /repos/{owner}/{repo}/issues?page={$index}", {
+        mensaje = await octokit.request("GET /repos/{owner}/{repo}/issues?page={$index}", {
             owner: "godotengine",
             repo: "godot",
-        }));
+        });
+
+        issues.push(mensaje.data);
+
 
     }
 
     return issues;
 }
-//paginacion
-const obtenerPaginacione = async () => {
+const crearJson = async () => {
+    try {
+        const issuesData = await obtenerIssues();
+        console.log(issuesData); // Imprimir los datos obtenidos
+        await fs.promises.writeFile("./thing.json", JSON.stringify(issuesData, null, 2));
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
 
-
-}
-console.log(await obtenerIssues());
+crearJson();
 
